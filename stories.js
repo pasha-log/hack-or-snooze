68e1f938f73b9f3e9ff9c3ffbda72553e514a21f
Pasha Loguinov <pasha.log@gmail.com>
2022-08-25 11:54:48 -0700

    fixed bugs

--- stories.js
+++ stories.js
@@ -18,21 +18,22 @@ async function getAndShowStoriesOnStart() {
  *
  * Returns the markup for the story.
  */
-  // <span class="star">
-  //   <i class="far fa-star"></i>
-  // </span>
+// <span class="star">
+//   <i class="far fa-star"></i>
+// </span>
 
 function generateStoryMarkup(story) {
-
 	const hostName = story.getHostName();
-
-  const showTrash = Boolean((currentUser.ownStories).includes(story));
-  const showStar = Boolean(currentUser);
-
+	let showTrash = false;
+	let showStar = false;
+	if (currentUser) {
+		showTrash = Boolean(currentUser.ownStories.includes(story));
+		showStar = Boolean(currentUser);
+	}
 	return $(`
       <li id="${story.storyId}">
-      ${showTrash ? getDeleteBtnHTML() : ""}
-      ${showStar ? getStarHTML(story, currentUser) : ""}
+      ${showTrash ? getDeleteBtnHTML() : ''}
+      ${showStar ? getStarHTML(story, currentUser) : ''}
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -46,7 +47,7 @@ function generateStoryMarkup(story) {
 /** Make delete button HTML for story */
 
 function getDeleteBtnHTML() {
-  return `
+	return `
       <span class="trash-can">
         <i class="fas fa-trash-alt">
         </i>
@@ -56,9 +57,9 @@ function getDeleteBtnHTML() {
 /** Make favorite/not-favorite star for story */
 
 function getStarHTML(story, user) {
-  const isFavorite = user.isFavorite(story);
-  const starType = isFavorite ? "fas" : "far";
-  return `
+	const isFavorite = user.isFavorite(story);
+	const starType = isFavorite ? 'fas' : 'far';
+	return `
       <span class="star">
         <i class="${starType} fa-star"></i>
       </span>`;
@@ -87,7 +88,7 @@ $('#submit-story').on('click', async function() {
 	let author = $('#create-author').val();
 	let title = $('#create-title').val();
 	let url = $('#create-url').val();
-	await storyList.addStory(currentUser, {title, author, url});
+	await storyList.addStory(currentUser, { title, author, url });
 	window.location.reload();
 });
 
@@ -101,7 +102,7 @@ $('#submit-story').on('click', async function() {
 
 $favoritedStories.hide();
 function putFavoritesListOnPage() {
-  $favoritedStories.empty();
+	$favoritedStories.empty();
 	if (currentUser.favorites.length === 0) {
 		$favoritedStories.append('<h1>No Favorites Added!</h1>');
 	} else {
@@ -111,41 +112,42 @@ function putFavoritesListOnPage() {
 		}
 	}
 	$favoritedStories.show();
+  $favoritedStories.on('click', '.star', toggleStoryFavorite);
 }
 
 /** Handle favorite/un-favorite a story */
 
 async function toggleStoryFavorite(evt) {
-  const $targ = $(evt.target);
-  const $closestLi = $targ.closest('li');
-  const storyId = $closestLi.attr('id');
-  const story = storyList.stories.find(s => s.storyId === storyId);
-  if($targ.hasClass('fas')){
-    await currentUser.removeFavorite(story);
-    $targ.closest('i').toggleClass('fas far')
-  } else {
-    await currentUser.addFavorite(story);
-    $targ.closest('i').toggleClass('fas far')
-  }
+	const $targ = $(evt.target);
+	const $closestLi = $targ.closest('li');
+	const storyId = $closestLi.attr('id');
+	const story = storyList.stories.find((s) => s.storyId === storyId);
+	if ($targ.hasClass('fas')) {
+		await currentUser.removeFavorite(story);
+		$targ.closest('i').removeClass('far');
+	} else {
+		await currentUser.addFavorite(story);
+		$targ.closest('i').addClass('fas');
+	}
 }
 
-$allStoriesList.on('click', '.star', toggleStoryFavorite)
+$allStoriesList.on('click', '.star', toggleStoryFavorite);
 
-// Once I have submitted a story, add it to an array list of my submitted stories. 
-// Append each submitted story to the hidden submitted "#my-story" page the same way as the favorites. 
-// Only this time, add a little garbage can icon next to it. Create an event listener for the garbage to remove from 
-// list of submitted stories. 
+// Once I have submitted a story, add it to an array list of my submitted stories.
+// Append each submitted story to the hidden submitted "#my-story" page the same way as the favorites.
+// Only this time, add a little garbage can icon next to it. Create an event listener for the garbage to remove from
+// list of submitted stories.
 
 // Add HTML for My Stories page.
 
 $ownStories.hide();
 function putOwnStoriesOnPage() {
-  $ownStories.empty();
+	$ownStories.empty();
 	if (currentUser.ownStories.length === 0) {
 		$ownStories.append('<h1>No stories added by user yet!</h1>');
 	} else {
 		for (let userStory of currentUser.ownStories) {
-		  const $userStory = generateStoryMarkup(userStory);
+			const $userStory = generateStoryMarkup(userStory);
 			$ownStories.append($userStory);
 		}
 	}
@@ -155,15 +157,15 @@ function putOwnStoriesOnPage() {
 /** Handle deleting a story. */
 
 async function deleteStory(evt) {
-  console.debug("deleteStory");
+	console.debug('deleteStory');
 
-  const $closestLi = $(evt.target).closest("li");
-  const storyId = $closestLi.attr("id");
+	const $closestLi = $(evt.target).closest('li');
+	const storyId = $closestLi.attr('id');
 
-  await storyList.removeStory(currentUser, storyId);
+	await storyList.removeStory(currentUser, storyId);
 
-  // re-generate story list
-  await putOwnStoriesOnPage();
+	// re-generate story list
+	await putOwnStoriesOnPage();
 }
 
-$ownStories.on("click", ".trash-can", deleteStory);
+$ownStories.on('click', '.trash-can', deleteStory);
--- user.js
+++ user.js
@@ -25,6 +25,7 @@ async function login(evt) {
 
   saveUserCredentialsInLocalStorage();
   updateUIOnUserLogin();
+  location.reload();
 }
 
 $loginForm.on("submit", login);
